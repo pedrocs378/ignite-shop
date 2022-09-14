37c1165e8813d894a09c560703b39e773cf150f2
Pedro <pedrocs378@gmail.com>
2022-09-14 20:07:55 -0300

    feat: add header and base carousel itens styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,24 @@
 import { AppProps } from 'next/app'
+import Image from 'next/image'
+
+import logoImg from '../assets/logo.svg'
 
 import { globalStyles } from '../styles/global'
+
+import * as S from '../styles/pages/app'
 
 globalStyles()
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <S.Container>
+      <S.Header>
+        <Image src={logoImg.src} alt="" width={logoImg.width} height={logoImg.height} />
+      </S.Header>
+
+      <Component {...pageProps} />
+    </S.Container>
+  )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,30 @@
-import { styled } from '../styles'
+import Image from 'next/image'
 
-const Button = styled('button', {
-  backgroundColor: '$green500'
-})
+import camiseta1 from '../assets/camisetas/1.png'
+import camiseta2 from '../assets/camisetas/2.png'
+import camiseta3 from '../assets/camisetas/3.png'
+
+import * as S from '../styles/pages/home'
 
 export default function Home() {
   return (
-    <Button>Enviar</Button>
+    <S.HomeContainer>
+      <S.Product>
+        <Image src={camiseta1} alt="" width={520} height={400} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </S.Product>
+      <S.Product>
+        <Image src={camiseta2} alt="" width={520} height={400} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </S.Product>
+    </S.HomeContainer>
   )
 }
